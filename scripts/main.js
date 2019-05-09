a811813f36053d6076b0fdbabf4d1ca7b2fed298
orjon <github@orjon.com>
2019-05-09 15:38:17 +0100

    Whack-a-mole and Project4 added. Layout adjusted

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//
+//   const icons = document.querySelectorAll('.icon')
+//   const soundWoosh = document.querySelector('.soundWoosh')
+//
+//
+//   icons.forEach(name => {
+//     name.addEventListener('mouseover', () => {
+//       soundWoosh.playbackRate = 1
+//       soundWoosh.currentTime = 0
+//       soundWoosh.play()
+//     })
+//   })
+// })
