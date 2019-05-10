7f06648ff2f20ae17457c4c6468e3511b3029bd6
orjon <github@orjon.com>
2019-05-10 12:34:12 +0100

    O animation added

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,50 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//
-//   const icons = document.querySelectorAll('.icon')
-//   const soundWoosh = document.querySelector('.soundWoosh')
-//
-//
-//   icons.forEach(name => {
-//     name.addEventListener('mouseover', () => {
-//       soundWoosh.playbackRate = 1
-//       soundWoosh.currentTime = 0
-//       soundWoosh.play()
-//     })
-//   })
-// })
+window.addEventListener('DOMContentLoaded', () => {
+
+  const icons = document.querySelectorAll('.icon')
+  const soundWoosh = document.querySelector('.soundWoosh')
+
+  //
+  // const root = document.documentElement
+  // const style = document.documentElement.style
+  //
+  //
+  //
+  // function randomizeAnimDist() {
+  //   let animDist = Math.random()*100
+  //   console.log(animDist)
+  //   style.setProperty('--animDist1', animDist)
+  //   // root.style.setProperty('--animDist1', (animDist) + 'px')
+  //   // animDist = Math.random()*100
+  //   // console.log(animDist)
+  //   // root.style.setProperty('--animDist2', (animDist) + 'px')
+  //   // animDist = Math.random()*100
+  //   // console.log(animDist)
+  //   // root.style.setProperty('--animDist3', (animDist) + 'px')
+  //   // animDist = Math.random()*100
+  //   // console.log(animDist)
+  //   // root.style.setProperty('--animDist4', (animDist) + 'px')
+  //   // animDist = Math.random()*100
+  //   // console.log(animDist)
+  //   // root.style.setProperty('--animDist5', (animDist) + 'px')
+  //   // animDist = Math.random()*100
+  //   // console.log(animDist)
+  //   // root.style.setProperty('--animDist6', (animDist) + 'px')
+  // }
+  //
+  // setInterval(randomizeAnimDist,500)
+
+  icons.forEach(name => {
+    name.addEventListener('mouseover', () => {
+      soundWoosh.playbackRate = 1
+      soundWoosh.currentTime = 0
+      soundWoosh.play()
+    })
+  })
+
+
+
+
+
+
+
+})
