f728d6bbc213535cee3fa58703eb94543de52c90
orjon <github@orjon.com>
2019-05-15 17:01:48 +0100

    New intro and bi sections added V2.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,19 @@
+
 window.addEventListener('DOMContentLoaded', () => {
+
 
   const icons = document.querySelectorAll('.icon')
   const soundWoosh = document.querySelector('.soundWoosh')
-  // const basicScroll = require('basicscroll')
 
-  //
-  // const root = document.documentElement
-  // const style = document.documentElement.style
-  //
-  //
-  //
-  // function randomizeAnimDist() {
-  //   let animDist = Math.random()*100
-  //   console.log(animDist)
-  //   style.setProperty('--animDist1', animDist)
-  //   // root.style.setProperty('--animDist1', (animDist) + 'px')
-  //   // animDist = Math.random()*100
-  //   // console.log(animDist)
-  //   // root.style.setProperty('--animDist2', (animDist) + 'px')
-  //   // animDist = Math.random()*100
-  //   // console.log(animDist)
-  //   // root.style.setProperty('--animDist3', (animDist) + 'px')
-  //   // animDist = Math.random()*100
-  //   // console.log(animDist)
-  //   // root.style.setProperty('--animDist4', (animDist) + 'px')
-  //   // animDist = Math.random()*100
-  //   // console.log(animDist)
-  //   // root.style.setProperty('--animDist5', (animDist) + 'px')
-  //   // animDist = Math.random()*100
-  //   // console.log(animDist)
-  //   // root.style.setProperty('--animDist6', (animDist) + 'px')
-  // }
-  //
-  // setInterval(randomizeAnimDist,500)
 
-  icons.forEach(name => {
-    name.addEventListener('mouseover', () => {
-      soundWoosh.playbackRate = 1
-      soundWoosh.currentTime = 0
-      soundWoosh.play()
+  icons.forEach(icon => {
+    icon.addEventListener('mouseover', () => {
+      var sound = new Audio()
+      sound.src = soundWoosh.src
+      sound.playbackRate = 1
+      sound.currentTime = 0
+      // sound.play()
     })
   })
-
-  // const instance = basicScroll.create({
-  //   elem: document.getElementById('circleWhite'),
-  //   from: 'top-bottom',
-  //   to: 'middle-middle',
-  //   props: {
-  //     '--opacity': {
-  //       from: .99,
-  //       to: .5
-  //     }
-  //   }
-  // })
-  //
-  // instance.start()
-
-
 
 })
