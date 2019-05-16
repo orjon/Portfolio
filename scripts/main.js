a75aef7eb51a66453134ddb8a1e9d18e5dfa437f
orjon <github@orjon.com>
2019-05-16 01:16:03 +0100

    Responsiveness added V2.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
-
 window.addEventListener('DOMContentLoaded', () => {
-
 
   const icons = document.querySelectorAll('.icon')
   const soundWoosh = document.querySelector('.soundWoosh')
+
+  window.onload = function (){
+    checkWindowSize()
+  }
+
+  window.onresize = function(){
+    checkWindowSize()
+  }
+
+  function checkWindowSize(){
+    var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+
+    if (windowWidth < windowHeight){
+      document.documentElement.style.setProperty('--oSize', '70vw')
+      document.documentElement.style.setProperty('--fontSizeHello', '4vw')
+    } else {
+      document.documentElement.style.setProperty('--oSize', '70vh')
+      document.documentElement.style.setProperty('--fontSizeHello', '4vh')
+    }
+  }
 
 
   icons.forEach(icon => {
